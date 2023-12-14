1fda7f593e87df45e0237d1083df70ab602e6899
Lucas Gomes da Silva <silva.lucasgomes@protonmail.com>
2023-12-14 14:22:45 -0300

    removendo tudo da main

--- src/main/java/br/com/helpusz/HelpuszApplication.java
+++ src/main/java/br/com/helpusz/HelpuszApplication.java
@@ -1,27 +1,12 @@
 package br.com.helpusz;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.helpusz.model.Volunteer;
-import br.com.helpusz.service.VolunteerService;
-
 @SpringBootApplication
-public class HelpuszApplication implements CommandLineRunner {
+public class HelpuszApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpuszApplication.class, args);
-	}
-
-	@Autowired
-	private VolunteerService volunteerService;
-
-	Volunteer volunteer = new Volunteer("Lucas", "41991653248", "silva.lucasgomes@protonmail.com", "Senha123");
-
-	@Override
-	public void run(String... args) throws Exception {
-			this.volunteerService.save(this.volunteer);
 	}
 }

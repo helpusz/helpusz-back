5b8a74ea60edd0af9b8ac3375c52ed3d91178a5a
Lucas Gomes da Silva <silva.lucasgomes@protonmail.com>
2023-12-14 10:48:09 -0300

    criando projeto

---
+++ src/main/java/br/com/helpusz/HelpuszApplication.java
@@ -0,0 +1,13 @@
+package br.com.helpusz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpuszApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpuszApplication.class, args);
+	}
+
+}

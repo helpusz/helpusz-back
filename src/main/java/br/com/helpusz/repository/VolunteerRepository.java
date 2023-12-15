368790d42a5f30e89b32b8700148ca67afc53760
Lucas Gomes da Silva <silva.lucasgomes@protonmail.com>
2023-12-14 22:52:14 -0300

    findByEmail

--- src/main/java/br/com/helpusz/repository/VolunteerRepository.java
+++ src/main/java/br/com/helpusz/repository/VolunteerRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import br.com.helpusz.model.Volunteer;
 
+
 public interface VolunteerRepository extends MongoRepository<Volunteer, String> {
-  
+  Volunteer findByEmail(String email);
 }

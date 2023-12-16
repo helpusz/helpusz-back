ab6cb0e75a916a924c4081abf831a4eb414a7bc1
Lucas Gomes da Silva <silva.lucasgomes@protonmail.com>
2023-12-16 20:25:06 -0300

    recriando projeto com maven

---
+++ src/test/java/br/com/helpusz/helpusz/HelpuszApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.helpusz.helpusz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpuszApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

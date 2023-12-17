a7f34d8820f5042be2fdb232559ed15af8a57e2b
Lucas Gomes da Silva <silva.lucasgomes@protonmail.com>
2023-12-16 22:35:00 -0300

    criptografando a senha com o bcrypt

--- src/main/java/br/com/helpusz/helpusz/entities/Ong/OngServiceImpl.java
+++ src/main/java/br/com/helpusz/helpusz/entities/Ong/OngServiceImpl.java
@@ -1,6 +1,7 @@
 package br.com.helpusz.helpusz.entities.Ong;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,6 +9,8 @@ public class OngServiceImpl implements OngService {
 
   @Autowired
   private OngRepository ongRepository;
+
+  private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
   
   @Override
   public void register(Ong ong) {
@@ -15,6 +18,8 @@ public class OngServiceImpl implements OngService {
       throw new RuntimeException("Conta já existente");
     } 
 
+    ong.setPassword(passwordEncoder.encode(ong.getPassword()));
+    
     this.ongRepository.save(ong);
   }
 
@@ -26,7 +31,7 @@ public class OngServiceImpl implements OngService {
 
     Ong existingOng = this.ongRepository.findByEmail(ong.getEmail());
 
-    if(!existingOng.getPassword().equals(ong.getPassword())) {
+    if(!passwordEncoder.matches(ong.getPassword(), existingOng.getPassword())) {
       throw new RuntimeException("Senha inválida");
     }
 

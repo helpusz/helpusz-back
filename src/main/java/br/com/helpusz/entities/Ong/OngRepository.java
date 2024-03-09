2323459447ddc6073ccdbc3609e04fc0d7719c89
Lucas Gomes da Silva <silva.lucasgomes@protonmail.com>
2024-03-09 20:34:00 -0300

    att os repositorys para receberem um objeto Email, e não uma String

--- src/main/java/br/com/helpusz/entities/Ong/OngRepository.java
+++ src/main/java/br/com/helpusz/entities/Ong/OngRepository.java
@@ -2,10 +2,12 @@ package br.com.helpusz.entities.Ong;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import br.com.helpusz.entities.Utils.Email;
+
 public interface OngRepository extends MongoRepository<Ong, String> {
 
-  boolean existsByEmail(String email);
+  boolean existsByEmail(Email email);
 
-  Ong findByEmail(String email);
+  Ong findByEmail(Email email);
   
 }
--- src/main/java/br/com/helpusz/entities/Volunteer/VolunteerRepository.java
+++ src/main/java/br/com/helpusz/entities/Volunteer/VolunteerRepository.java
@@ -2,10 +2,12 @@ package br.com.helpusz.entities.Volunteer;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import br.com.helpusz.entities.Utils.Email;
+
 public interface VolunteerRepository extends MongoRepository<Volunteer, String> {
 
-  boolean existsByEmail(String email);
+  boolean existsByEmail(Email email);
 
-  Volunteer findByEmail(String email);
+  Volunteer findByEmail(Email email);
   
 }

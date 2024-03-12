640dd286164c27dc2f63d51c9141a2b9b02a6889
Lucas Gomes da Silva <silva.lucasgomes@protonmail.com>
2024-03-11 21:22:37 -0300

    add @ContextConfiguration

--- src/test/java/br/com/helpusz/helpusz/HelpuszApplicationTests.java
+++ src/test/java/br/com/helpusz/helpusz/HelpuszApplicationTests.java
@@ -2,8 +2,13 @@ package br.com.helpusz.helpusz;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+
+import br.com.helpusz.HelpuszApplication;
 
 @SpringBootTest
+@ContextConfiguration(classes = {HelpuszApplication.class, TestConfiguration.class})
 class HelpuszApplicationTests {
 
 	@Test

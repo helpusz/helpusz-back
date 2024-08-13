6ac99a9534a042ca459e728f2374eb0acf1a7bf7
Lucas Gomes da Silva <silva.lucasgomes@protonmail.com>
2024-08-12 21:55:55 -0300

    Permitindo passar o HTTP Status da Exception

--- src/main/java/br/com/helpusz/exception/HelpuszException.java
+++ src/main/java/br/com/helpusz/exception/HelpuszException.java
@@ -1,15 +1,23 @@
 package br.com.helpusz.exception;
 
+import org.springframework.http.HttpStatus;
+
 public class HelpuszException extends RuntimeException {
-  
+
   private static final long serialVersionUID = 1L;
+  private final HttpStatus status;
 
-  public HelpuszException(String message) {
+  public HelpuszException(String message, HttpStatus status) {
     super(message);
+    this.status = status;
   }
 
-  public HelpuszException(String message, Throwable cause) {
+  public HelpuszException(String message, Throwable cause, HttpStatus status) {
     super(message, cause);
+    this.status = status;
   }
-  
+
+  public HttpStatus getStatus() {
+    return status;
+  }
 }

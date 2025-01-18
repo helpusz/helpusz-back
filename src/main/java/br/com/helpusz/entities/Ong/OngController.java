6d62d419bf08b4ee7debc762261643364c9f4bd3
Lucas Gomes da Silva <silva.lucasgomes@protonmail.com>
2025-01-18 10:04:08 -0300

    ♻️ (OngController): Alterando o tipo do binding do parametro de @RequestBody para @RequestParam

--- src/main/java/br/com/helpusz/entities/Ong/OngController.java
+++ src/main/java/br/com/helpusz/entities/Ong/OngController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,9 +26,9 @@ public class OngController {
   }
 
 	@GetMapping("/getAllByCategory")
-  public List<User> getAllByCategory(@RequestBody OngCategoryEnum category) {
-    return this.ongService.getAllOngsByCategory(category);
-  }
+	public List<User> getAllByCategory(@RequestParam OngCategoryEnum category) {
+		return this.ongService.getAllOngsByCategory(category);
+	}
 
 	@PostMapping("/validate")
   public void validate(@AuthenticationPrincipal User ong, @RequestBody String validationCode) {

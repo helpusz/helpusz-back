0ef470608cd07dac5c6b8d3a5cc745ab3f17ba45
Lucas Gomes da Silva <silva.lucasgomes@protonmail.com>
2025-02-09 23:21:14 -0300

    🚚  (CNPJTeste & EmailTest): Centralizando arquivos de teste na mesma pasta

--- src/test/java/br/com/helpusz/helpusz/CNPJTest.java
+++ src/test/java/br/com/helpusz/helpusz/CNPJTest.java
@@ -1,4 +1,4 @@
-package br.com.helpusz.helpusz.entities.Utils;
+package br.com.helpusz.helpusz;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
--- src/test/java/br/com/helpusz/helpusz/EmailTest.java
+++ src/test/java/br/com/helpusz/helpusz/EmailTest.java
@@ -1,4 +1,4 @@
-package br.com.helpusz.helpusz.entities.Utils;
+package br.com.helpusz.helpusz;
 
 import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
